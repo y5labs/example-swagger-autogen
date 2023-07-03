0aae531b09f4d7e13552db254c9db8fdb6500658
Thomas Coats <thomas@y5.nz>
2023-07-03 22:48:11 +1200

    ES6 + ctx

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-/*
- * Run the project and access the documentation at: http://localhost:3000/doc
- *
- * Use the command below to generate the documentation without starting the project:
- * $ npm start
- *
- * Use the command below to generate the documentation at project startup:
- * $ npm run start-gendoc
- */
+import swaggerUi from 'swagger-ui-express'
+import bodyParser from 'body-parser'
+import express from 'express'
+import endpoints from './src/endpoints.js'
+import { readFile } from 'fs/promises'
 
+const outputFile = './swagger-output.json'
+const swaggerFile = JSON.parse(await readFile(outputFile, 'utf-8'))
 
-const swaggerUi = require('swagger-ui-express')
-const swaggerFile = require('./swagger-output.json')
-const bodyParser = require('body-parser')
-const express = require('express')
 const app = express()
-
-/* Middlewares */
 app.use(bodyParser.json())
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use('/doc', swaggerUi.serve)
+app.get('/doc', swaggerUi.setup(swaggerFile))
+
+const ctx = { app }
+endpoints(ctx)
 
 app.listen(3000, () => {
   console.log("Server is running!\nAPI documentation: http://localhost:3000/doc")
 })
-
-/* Endpoints */
-require('./src/endpoints')(app)
--- src/users.js
+++ src/users.js
@@ -1,33 +1,32 @@
 
 function addUser(param) {
-    return true
+  return true
 }
 
 function getUser(param) {
-    const user = {
-        name: "Lewis Doe",
-        age: 32,
-        parents: {
-            father: "Simon Doe",
-            mother: "Marie Doe"
-        },
-        diplomas: [
-            {
-                school: "ABC University",
-                year: 2020,
-                completed: true,
-                internship: {
-                    hours: 310,
-                    location: "ABC Company"
-                }
-            }
-        ]
-    }
-    return user
+  const user = {
+    name: "Lewis Doe",
+    age: 32,
+    parents: {
+      father: "Simon Doe",
+      mother: "Marie Doe"
+    },
+    diplomas: [
+      {
+        school: "ABC University",
+        year: 2020,
+        completed: true,
+        internship: {
+          hours: 310,
+          location: "ABC Company"
+        }
+      }
+    ]
+  }
+  return user
 }
 
-
-module.exports = {
-    addUser,
-    getUser
+export {
+  addUser,
+  getUser
 }
